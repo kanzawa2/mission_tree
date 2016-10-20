7b2d21ef2fb1d17dbc9f15ed1768a139a0bd26cd
Kanzawa Hiroki <kanzawa@swlab.cs.okayama-u.ac.jp>
2016-10-20 17:18:49 +0900

    Modify to create mission table by render partial

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,30 +1,23 @@
 # coding: utf-8
 module MissionsHelper
-  def mission_traverse(node, creater)
-    str = []
-    return "" unless node
-    str << creater.make_row(node)
-    node.children.each do |child|
-      str << mission_traverse(child, creater)
+  def mission_traverse(mission)
+    return "" unless mission
+    str = visit_mission(mission)
+    mission.children.each do |child|
+      str += mission_traverse(child)
     end
-    node.tasks.each do |task|
-      str << creater.make_row(task)
+    mission.tasks.each do |task|
+      str += visit_task(task)
     end
-    
+
     str
   end
+
+  def visit_mission(mission)
+    render partial: "missions/table_row", locals: {mission: mission}
+  end
   
-  def make_table(roots, creater)
-    str = []
-    str << "<table #{creater.table_class}>"
-    str << creater.make_header
-    str << "<tbody>"
-    roots.each do |root|
-      str << mission_traverse(root, creater)
-    end
-    str << "</tbody>"
-    str << "</table>"
-    
-    str.join.html_safe
+  def visit_task(task)
+    render partial: "tasks/table_row", locals: {task: task}
   end
 end
