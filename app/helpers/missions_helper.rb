9084e0087a5ca2eef9d7b1c2beeec61aba457044
Kanzawa Hiroki <kanzawa@swlab.cs.okayama-u.ac.jp>
2016-10-21 17:34:18 +0900

    Modify to be able to specify any templates

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,23 +1,25 @@
 # coding: utf-8
 module MissionsHelper
-  def mission_traverse(mission)
+  def mission_traverse(mission, templates = {})
     return "" unless mission
-    str = visit_mission(mission)
+    m_tmp = templates[:m_tmp] || "table_row"
+    t_tmp = templates[:t_tmp] || "table_row"
+    str = visit_mission(mission, m_tmp)
     mission.children.each do |child|
-      str += mission_traverse(child)
+      str += mission_traverse(child, {m_tmp: m_tmp, t_tmp: t_tmp})
     end
     mission.tasks.each do |task|
-      str += visit_task(task)
+      str += visit_task(task, t_tmp)
     end
 
     str
   end
 
-  def visit_mission(mission)
-    render partial: "missions/table_row", locals: {mission: mission}
+  def visit_mission(mission, m_template)
+    render partial: "missions/#{m_template}", locals: {mission: mission}
   end
   
-  def visit_task(task)
-    render partial: "tasks/table_row", locals: {task: task}
+  def visit_task(task, t_template)
+    render partial: "tasks/#{t_template}", locals: {task: task}
   end
 end
